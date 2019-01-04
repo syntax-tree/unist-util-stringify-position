fea790b8e0a44059ea2b8e8ca80fbd50155cc057
Junyoung Choi <fluke8259@gmail.com>
2019-01-04 11:27:52 +0900

    Add types

---
+++ types/unist-util-stringify-position-tests.ts
@@ -0,0 +1,20 @@
+import stringify = require('unist-util-stringify-position')
+
+// Point
+const stringValue: string = stringify({line: 2, column: 3}) // => '2:3'
+
+// Position
+const stringValue2: string = stringify({
+  start: {line: 2, column: 1},
+  end: {line: 3, column: 1}
+}) // => '2:1-3:1'
+
+// Node
+const stringValue3: string = stringify({
+  type: 'text',
+  value: '!',
+  position: {
+    start: {line: 5, column: 11},
+    end: {line: 5, column: 12}
+  }
+}) // => '5:11-5:12'
