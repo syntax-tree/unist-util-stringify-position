6fbe41d47f459ff7923c1fdd5add986e32c0e271
Titus Wormer <tituswormer@gmail.com>
2021-04-13 17:23:50 +0200

    Add JSDoc based types

--- test.js
+++ test.js
@@ -13,16 +13,23 @@ test('stringifyPosition', function (t) {
     'should return empty `string` with `null`'
   )
   t.equal(
+    // @ts-ignore runtime.
     stringifyPosition('foo'),
     '',
     'should return empty `string` with `string`'
   )
   t.equal(
+    // @ts-ignore runtime.
     stringifyPosition(5),
     '',
     'should return empty `string` with `number`'
   )
-  t.equal(stringifyPosition({}), '', 'should return empty `string` with `{}`')
+  t.equal(
+    // @ts-ignore runtime.
+    stringifyPosition({}),
+    '',
+    'should return empty `string` with `{}`'
+  )
 
   t.equal(
     stringifyPosition({type: 'text'}),
@@ -31,6 +38,7 @@ test('stringifyPosition', function (t) {
   )
 
   t.equal(
+    // @ts-ignore runtime.
     stringifyPosition({type: 'text', position: 3}),
     '1:1-1:1',
     'should return a range for `node` with invalid `position` #1'
@@ -39,6 +47,7 @@ test('stringifyPosition', function (t) {
   t.equal(
     stringifyPosition({
       type: 'text',
+      // @ts-ignore runtime.
       position: {start: {}, end: {}}
     }),
     '1:1-1:1',
@@ -76,12 +85,14 @@ test('stringifyPosition', function (t) {
   )
 
   t.equal(
+    // @ts-ignore runtime.
     stringifyPosition({start: 3, end: 6}),
     '1:1-1:1',
     'should return a range for `position` with invalid `point`s #1'
   )
 
   t.equal(
+    // @ts-ignore runtime.
     stringifyPosition({start: {}, end: {}}),
     '1:1-1:1',
     'should return range for `position` with invalid `point`s #1'
@@ -112,18 +123,21 @@ test('stringifyPosition', function (t) {
   )
 
   t.equal(
+    // @ts-ignore runtime.
     stringifyPosition({line: 'foo', column: 'bar'}),
     '1:1',
     'should return a point for a `point` with invalid indices #1'
   )
 
   t.equal(
+    // @ts-ignore runtime.
     stringifyPosition({line: 4}),
     '4:1',
     'should return a point for a partially valid `point` #1'
   )
 
   t.equal(
+    // @ts-ignore runtime.
     stringifyPosition({column: 12}),
     '1:12',
     'should return a point for a partially valid `point` #1'
